ce2728f000f65b01b1cacbfc0f81990509ddb499
Thinh Nguyen <thinhntr@gmail.com>
2022-10-16 20:38:20 +0700

    Update pyenv installation script
    Changes:
    - Add -y option when installing build packages
    - Automatically intall python 3.10.8

--- notes/pyenv.sh
+++ notes/pyenv.sh
@@ -4,4 +4,7 @@
 # https://github.com/pyenv/pyenv-installer
 curl https://pyenv.run | bash
 
-sudo apt-get update; sudo apt-get install make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
+sudo apt-get update; sudo apt-get install make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev -y
+
+pyenv install 3.10.8
+
