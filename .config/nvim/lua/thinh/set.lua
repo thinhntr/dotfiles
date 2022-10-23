23270345d04c7ca33459e4ad71e04e48ce16ccf6
Thinh Nguyen <thinhntr@gmail.com>
2022-10-23 20:22:10 +0700

    Update tab settings

--- .config/nvim/lua/thinh/set.lua
+++ .config/nvim/lua/thinh/set.lua
@@ -3,7 +3,9 @@ vim.opt.relativenumber = true
 vim.opt.hlsearch = false
 vim.opt.scrolloff = 8
 
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
+-- tab
 vim.opt.expandtab = true
+vim.opt.smarttab = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 0
