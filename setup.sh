206bcb47ea82333158df23b80b1479dfcaca4978
Thinh Nguyen <thinhntr@gmail.com>
2024-02-11 17:54:21 +0700

    setup tmux

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@
 
 stow --target="$HOME" nvim -R
 stow --target="$HOME" bin -R
-stow --target="$HOME" alacritty -R
 stow --target="$HOME" zsh -R
+stow --target="$HOME" tmux -R
+stow --target="$HOME" alacritty -R
 stow --target="$HOME" i3 -R
