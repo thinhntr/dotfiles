db18e187a8f007e29aef85a9e41d511d314985f4
Thinh Nguyen <thinhntr@gmail.com>
2024-02-19 07:53:31 +0700

    nvim tmux find session

--- nvim/.config/nvim/lua/null/keymaps.lua
+++ nvim/.config/nvim/lua/null/keymaps.lua
@@ -1,6 +1,8 @@
 vim.keymap.set("n", "<Space>", "<Nop>", { silent = true })
 vim.keymap.set("n", "<leader>e", "<CMD>Oil<CR>")
 
+vim.keymap.set("n", "<leader>fp", '<cmd>silent !tmux neww tses<cr>')
+
 vim.keymap.set("n", "gl", "<cmd>lua vim.diagnostic.open_float()<cr>")
 vim.keymap.set("n", "[d", "<cmd>lua vim.diagnostic.goto_prev()<cr>")
 vim.keymap.set("n", "]d", "<cmd>lua vim.diagnostic.goto_next()<cr>")
