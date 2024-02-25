19b503ac3b74c6b43c2e05e2d8117c8632405f19
Thinh Nguyen <thinhntr@gmail.com>
2024-02-25 13:08:13 +0700

    starship

--- setup.sh
+++ setup.sh
@@ -4,5 +4,6 @@ stow --target="$HOME" nvim -R
 stow --target="$HOME" bin -R
 stow --target="$HOME" zsh -R
 stow --target="$HOME" tmux -R
+stow --target="$HOME" starship -R
 stow --target="$HOME" alacritty -R
 stow --target="$HOME" i3 -R
