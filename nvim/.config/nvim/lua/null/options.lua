660c3d47898c45f2678d578d128aafde5211f68a
Thinh Nguyen <thinhntr@gmail.com>
2024-03-03 18:52:40 +0700

    nvim-tree

--- nvim/.config/nvim/lua/null/options.lua
+++ nvim/.config/nvim/lua/null/options.lua
@@ -3,6 +3,8 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
 vim.g.netrw_banner = 0
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
 
 -- disable default providers
 for _, provider in ipairs({ "python3", "ruby", "node", "perl" }) do
