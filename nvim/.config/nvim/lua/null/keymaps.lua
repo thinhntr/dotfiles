df39b18700bf30c45d6625642f783cc7248ab93c
Thinh Nguyen <thinhntr@gmail.com>
2024-03-21 09:07:12 +0700

    quickfix keymap

--- nvim/.config/nvim/lua/null/keymaps.lua
+++ nvim/.config/nvim/lua/null/keymaps.lua
@@ -17,3 +17,9 @@ vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzz")
 vim.keymap.set("n", "N", "Nzz")
+
+-- quickfix
+vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
+vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
