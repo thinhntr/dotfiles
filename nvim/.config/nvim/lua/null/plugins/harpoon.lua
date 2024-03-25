3a0a103becc683f14e4af00ed96f653bdd154a86
Thinh Nguyen <thinhntr@gmail.com>
2024-03-25 07:24:59 +0700

    harpoon lualine

--- nvim/.config/nvim/lua/null/plugins/harpoon.lua
+++ nvim/.config/nvim/lua/null/plugins/harpoon.lua
@@ -1,55 +1,80 @@
 return {
-  "ThePrimeagen/harpoon",
-  branch = "harpoon2",
-  dependencies = { "nvim-lua/plenary.nvim" },
-  opts = {},
-  keys = function()
-    local harpoon = require("harpoon")
-    return {
+  {
+    "ThePrimeagen/harpoon",
+    branch = "harpoon2",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    opts = {},
+    keys = function()
+      local harpoon = require("harpoon")
+      return {
+        {
+          "<leader><leader>",
+          function()
+            harpoon.ui:toggle_quick_menu(harpoon:list())
+          end,
+          mode = { "n" },
+          desc = "Harpoon menu",
+        },
+        {
+          "<leader>a",
+          function()
+            harpoon:list():append()
+          end,
+          mode = { "n" },
+          desc = "Harpoon append",
+        },
+        {
+          "<leader>1",
+          function()
+            harpoon:list():select(1)
+          end,
+          mode = { "n" },
+        },
+        {
+          "<leader>2",
+          function()
+            harpoon:list():select(2)
+          end,
+          mode = { "n" },
+        },
+        {
+          "<leader>3",
+          function()
+            harpoon:list():select(3)
+          end,
+          mode = { "n" },
+        },
+        {
+          "<leader>4",
+          function()
+            harpoon:list():select(4)
+          end,
+          mode = { "n" },
+        },
+        {
+          "<leader>5",
+          function()
+            harpoon:list():select(5)
+          end,
+          mode = { "n" },
+        },
+        {
+          "<leader>6",
+          function()
+            harpoon:list():select(6)
+          end,
+          mode = { "n" },
+        },
+      }
+    end,
+  },
+  {
+    "letieu/harpoon-lualine",
+    dependencies = {
       {
-        "<leader><leader>",
-        function()
-          harpoon.ui:toggle_quick_menu(harpoon:list())
-        end,
-        mode = { "n" },
-        desc = "Harpoon menu",
+        "ThePrimeagen/harpoon",
+        branch = "harpoon2",
       },
-      {
-        "<leader>a",
-        function()
-          harpoon:list():append()
-        end,
-        mode = { "n" },
-        desc = "Harpoon append",
-      },
-      {
-        "<leader>1",
-        function()
-          harpoon:list():select(1)
-        end,
-        mode = { "n" },
-      },
-      {
-        "<leader>2",
-        function()
-          harpoon:list():select(2)
-        end,
-        mode = { "n" },
-      },
-      {
-        "<leader>3",
-        function()
-          harpoon:list():select(3)
-        end,
-        mode = { "n" },
-      },
-      {
-        "<leader>4",
-        function()
-          harpoon:list():select(4)
-        end,
-        mode = { "n" },
-      },
-    }
-  end,
+    },
+  },
 }
