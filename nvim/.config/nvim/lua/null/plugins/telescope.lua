4ee5dfc8f249d95aea652de5d8723c2328fcf96e
Thinh Nguyen <thinhntr@gmail.com>
2024-03-30 11:48:44 +0700

    telescope live-grep-args

--- nvim/.config/nvim/lua/null/plugins/telescope.lua
+++ nvim/.config/nvim/lua/null/plugins/telescope.lua
@@ -3,73 +3,59 @@ return {
   branch = "0.1.x",
   dependencies = {
     "nvim-lua/plenary.nvim",
+    "nvim-telescope/telescope-live-grep-args.nvim",
     {
       "nvim-telescope/telescope-fzf-native.nvim",
       build = "make",
       enabled = vim.fn.executable("make") == 1,
     },
   },
-  opts = function()
-    local grep_args = require("telescope.config").values.vimgrep_arguments
-    vim.list_extend(grep_args, { "--hidden", "-g=!.git" })
-    return grep_args
+  config = function()
+    local lga_actions = require("telescope-live-grep-args.actions")
+
+    require("telescope").setup({
+      extensions = {
+        live_grep_args = {
+          auto_quoting = true, -- enable/disable auto-quoting
+          -- define mappings, e.g.
+          mappings = { -- extend mappings
+            i = {
+              ["<C-k>"] = lga_actions.quote_prompt(),
+              ["<C-i>"] = lga_actions.quote_prompt({ postfix = " --iglob " }),
+            },
+          },
+          -- ... also accepts theme settings, for example:
+          -- theme = "dropdown", -- use dropdown theme
+          -- theme = { }, -- use own theme spec
+          -- layout_config = { mirror=true }, -- mirror preview pane
+        },
+      },
+    })
+    pcall(require("telescope").load_extension, "fzf")
+    pcall(require("telescope").load_extension, "live_grep_args")
   end,
-  keys = {
-    {
-      "<leader>ff",
-      function()
-        vim.cmd.Telescope("find_files")
-      end,
-      mode = { "n" },
-      desc = "Find files",
-    },
-    {
-      "<leader>fa",
-      function()
-        vim.cmd.Telescope("find_files", "follow=true", "no_ignore=true", "hidden=true")
-      end,
-      mode = { "n" },
-      desc = "Find files including hidden",
-    },
-    {
-      "<leader>fg",
-      function()
-        vim.cmd.Telescope("git_files")
-      end,
-      mode = { "n" },
-      desc = "Fuzzy search through the output of git ls-files command, respects .gitignore",
-    },
-    {
-      "<leader>fw",
-      function()
-        vim.cmd.Telescope("live_grep")
-      end,
-      mode = { "n" },
-      desc = "Live grep",
-    },
-    {
-      "<leader>fb",
-      function()
-        vim.cmd.Telescope("buffers")
-      end,
-      mode = { "n" },
-      desc = "Find buffers",
-    },
-    {
-      "<leader>fh",
-      function()
-        vim.cmd.Telescope("help_tags")
-      end,
-      mode = { "n" },
-      desc = "Help tags",
-    },
-    {
-      "<leader>fz",
-      function()
-        vim.cmd.Telescope("current_buffer_fuzzy_find")
-      end,
-      mode = { "n" },
-      desc = "Current buffer fuzzy find"
-    },
-  },
+  keys = function()
+    local builtin = require("telescope.builtin")
+    local lga = require("telescope").extensions.live_grep_args
+
+    return {
+      { "<leader>ff", builtin.find_files, mode = { "n" }, desc = "Find files" },
+      {
+        "<leader>fa",
+        function()
+          builtin.find_files({ follow = true, hidden = true, no_ignore = true })
+        end,
+        mode = { "n" },
+        desc = "Find files including hidden",
+      },
+      { "<leader>fl", lga.live_grep_args, mode = { "n" }, desc = "Find files through git ls-files" },
+      { "<leader>fg", builtin.git_files, mode = { "n" }, desc = "Find files through git ls-files" },
+      { "<leader>fw", builtin.live_grep, mode = { "n" }, desc = "Live grep" },
+      { "<leader>fb", builtin.buffers, mode = { "n" }, desc = "Find buffers" },
+      { "<leader>fh", builtin.help_tags, mode = { "n" }, desc = "Help tags" },
+      { "<leader>fz", builtin.current_buffer_fuzzy_find, mode = { "n" }, desc = "Current buffer fuzzy find" },
+    }
+  end,
+  -- enterprise
+  -- Enterprise
 }
