4dce7711495ec81fda6e2fe1c205f8e735a26371
Thinh Nguyen <thinhntr@gmail.com>
2024-10-14 20:22:21 +0700

    lsp add buffer source

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -226,9 +226,10 @@ return {
             group_index = 0,
           },
           { name = "nvim_lsp" },
-          { name = "luasnip" },
-          { name = "path" },
           { name = "nvim_lsp_signature_help" },
+          { name = "luasnip" },
+          { name = "buffer" },
+          { name = "path" },
         },
       })
     end,
